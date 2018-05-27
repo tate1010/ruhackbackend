eccedbeac94e4787c327c5fcb063bb1051de1730
dannykong12 <dannykong12@gmail.com>
2018-05-27 00:38:03 -0400

    heroku commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta, MO
 from flask import jsonify
 import urllib.request
+import urllib
 import json
 
 #def create_app(test_config=None):
@@ -37,10 +38,32 @@ app = Flask(__name__, instance_relative_config=True)
 #except OSError:#
 #    pass
 
-# a simple page that says hello
+
+@app.route('/<userID>/<query>')
+def parse(userID, query):
+    key="AIzaSyB8QIFggqzXTSoUU3qD0oN_6aXxe64ZewU"
+    keywords = {
+        "radius":"1000"
+    }
+    url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyB8QIFggqzXTSoUU3qD0oN_6aXxe64ZewU&'
+    queryDict = {x.split("=")[0]:x.split("=")[1] for x in query.split("&")}
+    for keyword in [item for item in keywords if item not in queryDict]:
+        queryDict[keyword]=keywords[keyword]
+    queryString = "&".join([key+"="+value for (key,value) in queryDict.items()])
+    url = url + str(queryString)
+
+
+    #contents = urllib.request.urlopen(url).read()
+
+    contents = json.load( urllib.request.urlopen(url.replace(" ", "%20")))
+    nextpage = contents["next_page_token"]
+
+    return contents
+
+
+
 @app.route('/hello')
 def hello():
-
         df = pd.read_excel('spending.xlsx',skiprows= 1)
         budget = 100
 
@@ -62,28 +85,16 @@ def hello():
             h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
             return m, m-h, m+h
 
-
         mean, lower , upper = mean_confidence_interval(food["DEBIT"])
-
         date_remaining = 6 -  (datetime.datetime.today().weekday())
-
-
         today = date.today()
         last_monday = today + relativedelta(weekday=MO(-1))
         print(last_monday)
-
         this_week_spending =  food.loc[food["DATE"]>= str(last_monday)]
-
         budget_remaining = budget - this_week_spending["DEBIT"].sum()
-
         budget_remaining = str(budget_remaining)
-
         result = {"budget_remaining" :  budget_remaining}
-
-
-
         queryresult = urllib.request(query(43.6576585,-79.3809904))
-
         return jsonify(result)
 
 def query(lat,lon):
@@ -98,17 +109,13 @@ def query(lat,lon):
     query = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?types=restaurant"
     query = appendquery(query,[key,location,radius])
     return query
+
 def appendquery(q,item):
     for i in item:
         q =  q + "&" + i
     return q
+
 if __name__ == '__main__':
-    #app.run(debug=True, use_reloader=True)
-    print(query(43.6576585,-79.3809904))
-    queryresult = json.load(urllib.request.urlopen(query(43.6576585,-79.3809904)))
-    nextpage = queryresult["next_page_token"]
-    #print(nextpage)
-    queryresult = queryresult["results"]
-    for item in queryresult:
-        print (item["name"])
-        print (str(item["geometry"]["location"]["lat"]) + "." + str(item["geometry"]["location"]["lng"]))
+    app.run(debug=True, use_reloader=True)
+
+    # queryresult = json.load(urllib.request.urlopen(query(43.6576585,-79.3809904)))
